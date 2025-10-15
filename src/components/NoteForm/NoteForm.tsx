add323bb137530742cd555eecfd85c8ffbf2de36
Faina K <fayina1@gmail.com>
2025-10-15 13:31:20 +0200

    fix Yup

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -9,7 +9,7 @@ import toast from "react-hot-toast";
 
 const NoteFormScema = Yup.object().shape({
   title: Yup.string()
-    .min(3, "Title must be at least 2 characters")
+    .min(3, "Title must be at least 3 characters")
     .max(50, "Title is too long")
     .required("Title is required"),
   content: Yup.string().max(500, "Content is too long"),
